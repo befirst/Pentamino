779a78eecb65d628040cf95741458939cd8cb04a
Denis <den-uln@mail.ru>
2016-10-05 14:25:25 +0400

    Чтение из файла

---
+++ PentaminoConsole/PentaminoConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PentaminoConsole
+{
+    class Program
+    {
+        static int CheckPentamino(int[,] pentamino, int[,] pic)
+        {
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            char[,] source;
+            System.Console.WriteLine("Please insert path to file or 0 for using default path: ");
+            string path = System.Console.ReadLine();
+            int[,] pentamino1 = { {1,1,1 }, {1,0,0 }, {1,0,0 } };
+            if (path == "0")
+                source = SourceReader.GetSource("1.in");
+            else
+                source = SourceReader.GetSource(path);
+            int[,] binarySource = new int[source.GetLength(0), source.GetLength(1)];
+            for (int i = 0; i < source.GetLength(0); i++)
+            {
+                for (int j = 0; j < source.GetLength(1); j++)
+                {
+                    if (source[i, j] == 'o')
+                        binarySource[i, j] = 1;
+                    else
+                        binarySource[i, j] = 0;
+                    System.Console.Write(binarySource[i, j] + "");
+                }
+                System.Console.WriteLine();
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ PentaminoConsole/PentaminoConsole/SourceReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PentaminoConsole
+{
+    class SourceReader
+    {
+        private static SourceReader sr = new SourceReader();
+        private static string[] stringResultArray;
+        private static char[,] resultArray;
+        private SourceReader() { }
+        public static char[,] GetSource(string path)
+        {
+            string temp = "";
+            int rowCount = 0;
+            int columnCount = 0;
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            using (StreamReader reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    if(rowCount==0)
+                        temp += reader.ReadLine();
+                    else
+                    temp += "\n" + reader.ReadLine();
+                    rowCount++;
+                }
+            }
+            stringResultArray = temp.Split('\n');
+            foreach (var i in stringResultArray)
+                if (columnCount < i.Length)
+                    columnCount = i.Length;
+            resultArray = new char[rowCount, columnCount];
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                {
+                    if (j < stringResultArray[i].Length)
+                        resultArray[i, j] = stringResultArray[i][j];
+                    else
+                        resultArray[i, j] = ' ';
+                }
+            return resultArray;
+        }
+    }
+}
