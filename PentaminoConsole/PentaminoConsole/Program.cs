89f01b7488b26b325e2dab0c3f39251e44975371
Denis <den-uln@mail.ru>
2016-10-05 15:40:46 +0400

    Простейшая проверка количества вхождений пентамины в заданный рисунок

--- PentaminoConsole/PentaminoConsole/Program.cs
+++ PentaminoConsole/PentaminoConsole/Program.cs
@@ -9,10 +9,36 @@ namespace PentaminoConsole
 {
     class Program
     {
-        static int CheckPentamino(int[,] pentamino, int[,] pic)
+        static int CheckPentamino(int[,] pentamino, int[,] figure)
         {
+            int count = 0;//количество положений в котором может распологаться пентамино
+            for (int y = 0; y < figure.GetLength(0) - pentamino.GetLength(0)+1; y++)
+                for (int x = 0; x < figure.GetLength(1) - pentamino.GetLength(1)+1; x++)
+                {
+                    bool flag = true;
+                    int g = 0;
+                    //новое положение 
+                    for (int i = 0; i < pentamino.GetLength(0); i++)
+                        for (int j = 0; j < pentamino.GetLength(1); j++)
+                        {
+                            if(pentamino[i, j] == 1 && figure[i + y, j + x] == 1)
+                            //поменять значения в dancinglinks, запомнить положение
+                            {
+                                flag = true;
+                                g++;
+                            }
+                            else
+                            {
+                                flag = false;                                
+                                //break;
+                            }
 
-            return 0;
+                        }
+                    if (g==5)
+                        count++;
+                }
+
+            return count;
         }
 
         static void Main(string[] args)
@@ -20,7 +46,10 @@ namespace PentaminoConsole
             char[,] source;
             System.Console.WriteLine("Please insert path to file or 0 for using default path: ");
             string path = System.Console.ReadLine();
-            int[,] pentamino1 = { {1,1,1 }, {1,0,0 }, {1,0,0 } };
+            int[,] pentamino1 = { {1,1,1 },
+                                  {1,0,0 },
+                                  {1,0,0 } };
+            //сделать выполнение для каждого файла в базовой директории
             if (path == "0")
                 source = SourceReader.GetSource("1.in");
             else
@@ -38,6 +67,7 @@ namespace PentaminoConsole
                 }
                 System.Console.WriteLine();
             }
+            System.Console.WriteLine(CheckPentamino(pentamino1, binarySource));
             System.Console.ReadLine();
         }
     }
